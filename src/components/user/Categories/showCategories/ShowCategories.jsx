8d3ce52d9b026a3f9a9d7c3a25e8b264e8425770
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2025-02-15 17:45:32 +0200

    Fixed loader

--- src/components/user/Categories/showCategories/ShowCategories.jsx
+++ src/components/user/Categories/showCategories/ShowCategories.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import useFetch from '../../../../customHooks/useFetch'
-import Loader from '../../Loader/Loader';
 import Category from '../category/Category';
 import {  Container, Row } from 'react-bootstrap';
+import Loader from '../../Loader/Loader';
 
 export default function ShowCategories() {
   const {error,data,isLoading} = useFetch("https://ecommerce-node4.onrender.com/categories/active");
