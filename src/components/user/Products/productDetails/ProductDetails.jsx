cd1922bc95ed75bb034308eaca4cf31015aaf7e2
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2025-02-26 01:16:31 +0200

    replaced loader

--- src/components/user/Products/productDetails/ProductDetails.jsx
+++ src/components/user/Products/productDetails/ProductDetails.jsx
@@ -1,18 +1,24 @@
 import { Link, Outlet, useParams } from 'react-router-dom'
 import useFetch from '../../../../customHooks/useFetch';
-import Loader from '../../Loader/Loader';
+// import Loader from '../../Loader/Loader';
 import Swal from 'sweetalert2';
 import Details from './Details';
 import { Container } from 'react-bootstrap';
 import style from "./Details.module.css"
 import { useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+
 
 export default function ProductDetails() {
     const [activePage, setActivePage] = useState("desc");    
     const {productId} = useParams();
     const {error,data,isLoading} =  useFetch(`${import.meta.env.VITE_BURL}/products/${productId}`)
     if (isLoading) {
-        return <Loader />;
+        return   (<section className="loader d-flex align-items-center justify-content-center">
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+            </section>)
     }
 
     if(error) {
