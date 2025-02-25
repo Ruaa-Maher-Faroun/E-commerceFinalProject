d366119b084b32cbdd1d94ea843adc7029136dc9
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2025-02-26 00:57:05 +0200

    trying to fix loader

--- src/pages/user/products/Products.jsx
+++ src/pages/user/products/Products.jsx
@@ -1,16 +1,22 @@
-import Loader from '../../../components/user/Loader/Loader';
+// import Loader from '../../../components/user/Loader/Loader';
 import React from 'react'
 import ShowProducts from '../../../components/user/Products/showProducts/ShowProducts'
 import { Container } from 'react-bootstrap';
 import style from "./productsPage.module.css";
 import ErrorsPage from '../errorsPage/ErrorsPage';
 import useFetch from '../../../customHooks/useFetch';
+import Spinner from 'react-bootstrap/Spinner';
+
 export default function Products() {
   
   const { error, data, isLoading } = useFetch(`${import.meta.env.VITE_BURL}/products?limit=10`);
 
   if (isLoading) {
-    return <Loader />;
+    return    (<section className="loader d-flex align-items-center justify-content-center">
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+    </section>)
   }
 
   if (error) {
