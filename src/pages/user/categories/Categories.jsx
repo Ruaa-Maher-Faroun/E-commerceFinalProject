84b928514d130b09e352a95e4ba7d20dfc8c3219
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2025-02-26 00:52:02 +0200

    trying to fix loader

--- src/pages/user/categories/Categories.jsx
+++ src/pages/user/categories/Categories.jsx
@@ -1,9 +1,9 @@
+import Loader from '../../../components/user/Loader/Loader';
 import React from 'react'
 import {  Container, Row } from 'react-bootstrap';
 import useFetch from '../../../customHooks/useFetch';
 import Category from '../../../components/user/Categories/category/Category';
 import ErrorsPage from '../errorsPage/ErrorsPage';
-import Loader from '../../../components/user/Loader/Loader';
 
 export default function Categories() {
   const {error,data,isLoading} = useFetch(`${import.meta.env.VITE_BURL}/categories/active`);
