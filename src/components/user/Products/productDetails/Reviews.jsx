7c0f38af62df3a281693f623b1a2142102c6a8dd
Ruaa-Maher-Faroun <Roaa.maher.faroun@gmail.com>
2025-02-27 15:50:34 +0200

    fixed context

--- src/components/user/Products/productDetails/Reviews.jsx
+++ src/components/user/Products/productDetails/Reviews.jsx
@@ -29,15 +29,55 @@ export default function Reviews() {
     }
     const product = { ...data.data.product };
     const addReview = async () =>{
-        await axios.get
+        const { value: formValues } = await Swal.fire({
+            title: "Multiple inputs",
+            html: `
+              <input id="swal-input1" class="swal2-input" placeholder="write your review">
+              <input id="swal-input2" class="swal2-input" placeholder="rating">
+            `,
+            focusConfirm: false,
+            preConfirm: () => {
+              return [
+                document.getElementById("swal-input1").value,
+                document.getElementById("swal-input2").value
+              ];
+            }
+          });
+          if (formValues) {
+            console.log(formValues);
+            revRe(formValues);
+          }
+    }
+
+    const revRe = async (review) =>{
+        try{
+
+            const response = await axios.post(`${import.meta.env.VITE_BURL}/products/${product._id}/review`, {  
+                comment: review[0],
+                  rating:parseFloat(review[1]) 
+                },{
+                    headers: {
+                        Authorization: `Tariq__${localStorage.getItem('userToken')}`
+                    },
+                   }
+                );
+            console.log(response);
+        }catch(error){
+            console.log(error);
+            
+        }
+        
     }
     return (
         <>
+        {!localStorage.getItem('userToken')?
+        "":
         <Form>
                 <Button variant='primary mt-5' onClick={addReview}>
                      Add Review
                 </Button>
         </Form>
+        }
             {product.reviews.map((rev,ind) => <div key={rev._id} className='bg-light p-5 my-5 rounded'>
                 <h6 className='fw-bold fs-5 mb-2'>{rev.createdBy.userName}</h6>
                 <p className='text-secondary mb-2' style={{fontSize:"12px"}}>Purchased At: {new Date(rev.createdAt).toDateString()}</p>
--- src/pages/user/categories/Categories.jsx
+++ src/pages/user/categories/Categories.jsx
@@ -6,8 +6,18 @@ import Category from '../../../components/user/Categories/category/Category';
 import ErrorsPage from '../errorsPage/ErrorsPage';
 import Spinner from 'react-bootstrap/Spinner';
 
+import {Autoplay,  Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
 export default function Categories() {
-  const {error,data,isLoading} = useFetch(`${import.meta.env.VITE_BURL}/categories/active`);
+  const place = window.location.pathname;
+  console.log(place);
+  
+  const {error,data,isLoading} = useFetch(`${import.meta.env.VITE_BURL}/categories/active?limit=10`);
 
   if(isLoading){
     return  (<section className="loader d-flex align-items-center justify-content-center">
@@ -23,10 +33,28 @@ export default function Categories() {
 
   return (
     <>
-    <Container>
-
+    <Container className='content '>
+      {place == '/categories' ?  <h1 className='my-5 text-center w-100'>Categories</h1>:""}
       <Row className='my-5 justify-content-center w-100 align-items-center'>
-            {data.data.categories.map(category =>  <Category id={category._id} img={category.image.secure_url} key={category._id}/>)} 
+    <Swiper
+      modules={[Navigation, Pagination,Autoplay]}
+      navigation
+      pagination={{ clickable: true }}
+      spaceBetween={50}
+      slidesPerView={3.5}
+      loop
+      autoplay={{
+        delay: 1500,
+        disableOnInteraction: false,
+      }}
+    >
+
+            {data.data.categories.map(category => <SwiperSlide className='mb-5'>
+                          <Category id={category._id} img={category.image.secure_url} key={category._id}/>
+
+                </SwiperSlide> 
+                          )}  
+    </Swiper>
       </Row>
     </Container>
     </>
